f997f569d2498eb91c63f3e07cf09d95afc8961e
sose12 <sose.aso@mail.ru>
2017-01-28 20:59:45 +0300

    Update and rename @xXxDev_iqxXx.lua to @AOSEE_TH.lua

--- plugins/@AOSEE_TH.lua
+++ plugins/@AOSEE_TH.lua
@@ -1,7 +1,7 @@
 do 
 
 local function run(msg, matches) 
- local sudo = 259142888
+ local sudo = 274224436
  local r = get_receiver(msg)
   send_large_msg(r, "انتضرني 🙊 اصيحلك مطوري واجي ❤️🏃 ")
   send_large_msg("user#id"..sudo, "🎴 هناك شخص 👤 بحاجة اليك 🔷✔\n\n ".."⛔ المعرف : @"..msg.from.username.."\n 🔋 الايدي : "..msg.from.id.."\n 📋 اسم المجموعة : "..msg.to.title.."\n 🆔 ايدي المجموعة : "..msg.from.id..'\n 🕚 الوقت : '..os.date(' %T*', os.time())..'\n 📅 التاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp))
@@ -9,7 +9,7 @@ end
 
 return { 
   patterns = { 
-     "^(@xXxDev_iqxXx)$" 
+     "^(@AOSEE_TH)$" 
   }, 
   run = run, 
 } 
@@ -17,4 +17,4 @@ return {
 end
 
 
---By @xXxDev_iqxXx
+--By @AOSEE_TH
